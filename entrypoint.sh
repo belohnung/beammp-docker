43fb18324c31e8f5a6d82f3e3e3ee7f3debdbcf2
belohnung <35181949+belohnung@users.noreply.github.com>
2023-09-18 01:34:04 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 cd /home/container
-
-ls
-
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
-
-# Run the Server
-${MODIFIED_STARTUP}
+./BeamMP-Server
