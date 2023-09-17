a4561dd77a66de9fb08c93faa06244b4104f1b35
blong <belohnung@protonmail.com>
2023-09-18 00:54:00 +0200

    ??

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+chmod +x /home/container/BeamMP-Server
 cd /home/container && MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')` && echo ":/home/container$ ${MODIFIED_STARTUP}" && ${MODIFIED_STARTUP}
